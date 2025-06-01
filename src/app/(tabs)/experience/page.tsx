e46494ac92989985c669513a99d85218324ed4f5
AlexFangSW <alexfangsw@gmail.com>
2025-06-01 21:41:42 +0800

    feat: basic layout for project and experience tab

--- src/app/(tabs)/experience/page.tsx
+++ src/app/(tabs)/experience/page.tsx
@@ -1,3 +1,12 @@
+import TabHeader from "@/components/TabHeader";
+
 export default function Page() {
-  return <>Experience</>;
+  return (
+    <div className={`center-container content-container tab-content-container`}>
+      <TabHeader title="EXPERIENCE" image="/tree.png" />
+      <main className={`center-container inner-content`}>
+        <p>Experience</p>
+      </main>
+    </div>
+  );
 }
--- src/app/(tabs)/projects/page.tsx
+++ src/app/(tabs)/projects/page.tsx
@@ -1,3 +1,12 @@
+import TabHeader from "@/components/TabHeader";
+
 export default function Page() {
-  return <>Projects</>;
+  return (
+    <div className={`center-container content-container tab-content-container`}>
+      <TabHeader title="PROJECTS" image="/tree.png" />
+      <main className={`center-container inner-content`}>
+        <p>PROJECTS</p>
+      </main>
+    </div>
+  );
 }
